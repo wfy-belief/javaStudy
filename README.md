f7b590cd6b5f4e1e00c611dc1fb50cc0857f55a0
wfy-belief <1335680234@qq.com>
2021-07-07 15:37:27 +0800

    第一个模块提交

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # javaStudy
+**记录自己暑假学习java的历程**
---
+++ src/com/wfybelief/demo0/Part1.java
@@ -0,0 +1,7 @@
+package com.wfybelief.demo0;
+
+public class Part1 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
